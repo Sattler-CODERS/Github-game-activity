e9983e4b762aab1abfa83d238d7b68da0f800a98
sethbam9 <54952081+sethbam9@users.noreply.github.com>
2021-11-11 15:33:38 -0500

    bullets

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -47,7 +47,7 @@ class Settings:
 
         self.set_default_alien_directions()
         self.ship_speed_factor = 400
-        self.bullet_speed_factor = 800
+        self.bullet_speed_factor = 10000
         self.alien_speed_factor_x = 250
         self.alien_speed_factor_y = 100
         self.alien_density_factor_x = 3
